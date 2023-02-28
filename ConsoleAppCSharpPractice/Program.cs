628758296f6c1e703a580cd88f480f3b06d0da88
Stephen Pino <Stephenp3123@gmail.com>
2023-02-28 11:11:03 +0000

    Add project files.

---
+++ ConsoleAppCSharpPractice/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Figure 1-1:
+var inputX = 10;
+var inputY = 20;
+
+if (inputX > inputY)
+{
+    Console.WriteLine(inputX.ToString());
+}
+
+Console.WriteLine(inputY.ToString());
+
+
